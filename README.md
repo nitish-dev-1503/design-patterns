71024a7b070b9859e4f0191252bd6cfe86326525
Nitish Sharma <smartnitish15@gmail.com>
2022-04-03 13:09:33 +0530

    Added prototype design pattern

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This repository contains code examples of different design patterns. The code is
 1. Factory Pattern
 2. Abstract Factory Pattern
 3. Builder Pattern
+4. Prototype Pattern
---
+++ src/main/java/com/design/patterns/prototype/Rectangle.java
@@ -0,0 +1,47 @@
+package com.design.patterns.prototype;
+
+public class Rectangle extends Shape {
+    private int length;
+    private int breadth;
+
+    public Rectangle(int x, int y) {
+        super(x, y);
+    }
+
+    public Rectangle(Rectangle source) {
+        super(source.getX(), source.getY());
+        this.length = source.length;
+        this.breadth = source.breadth;
+    }
+
+    @Override
+    public Shape clone() {
+        return new Rectangle(this);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    public void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", breadth=" + breadth +
+                ", x=" + getX() +
+                ", y=" + getY() +
+                "} ";
+    }
+}
